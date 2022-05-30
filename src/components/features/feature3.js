084dccf15b03158cdf61c7c417433184acee2ccf
pratik <pratik@pop-os.localdomain>
2022-05-30 19:33:08 +0530

    added features

--- src/components/features/feature3.js
+++ src/components/features/feature3.js
@@ -3,14 +3,14 @@ import "./features1.css";
 
 function Feature3({title, heading, body, image}) {
   return (
-    <div className="feature">
+    <div className="feature1">
       <div>
-        <h1 className="feature__title">{title}</h1>
-        <div className="feature__heading">{heading}</div>
-        <div className="feature__body">{body}</div>
+        <h1 className="feature1__title">{title}</h1>
+        <div className="feature1__heading">{heading}</div>
+        <div className="feature1__body">{body}</div>
       </div>
       <div>
-        <img src={image} alt="" className="feature__image"></img>
+        <img src={image} alt="" className="feature1__image"></img>
       </div>
     </div>
   );
